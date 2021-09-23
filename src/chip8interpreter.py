2423651dd168ebe0a30f3e4696f9afb559b070c0
Guille Polito <guillermopolito@gmail.com>
2021-09-23 11:48:43 +0200

    Adding graphic memory and sprite drawing

--- src/chip8interpreter.py
+++ src/chip8interpreter.py
@@ -1,3 +1,6 @@
+from src.byteDecoder import ByteDecoder
+
+
 class Chip8Interpreter:
 
     def __init__(self, program):
@@ -6,6 +9,7 @@ class Chip8Interpreter:
         self.PC = 0
         self.program = program
         self.registers = [0] * 16
+        self.graphic_memory = [0] * (64 * 32)
 
     def step(self):
         self.execute(self.program[self.PC: self.PC + 2])
@@ -23,6 +27,12 @@ class Chip8Interpreter:
             N1 = first_byte & 0x0F
             jump_target = (N1 << 1 * 8) + second_byte
             self.PC = jump_target
+        elif opcode == 0xD:
+            decoder = ByteDecoder(instruction_bytes)
+            vx = decoder.digit_at(1)
+            vy = decoder.digit_at(2)
+            height = decoder.digit_at(3)
+            self.draw_sprite(self.get_register(vx), self.get_register(vy), height, self.I)
         else:
             raise Exception("Undefined instruction")
 
@@ -35,3 +45,20 @@ class Chip8Interpreter:
         if register_id < 0:
             raise IndexError(register_id)
         return self.registers[register_id]
+
+    def get_pixel(self, x, y):
+        if x < 0:
+            raise IndexError(x)
+        if y < 0:
+            raise IndexError(y)
+        return self.graphic_memory[y * 64 + x]
+
+    def put_pixel(self, x, y, pixel_value):
+        self.graphic_memory[y * 64 + x] = pixel_value
+
+    def draw_sprite(self, x, y, height, sprite_address):
+        for h in range(0, height):
+            sprite_byte = self.program[sprite_address + h]
+            for i in range(1, 8):
+                pixel_value = (sprite_byte >> (8-i)) & 0x1
+                self.put_pixel(x + i - 1, y + h, pixel_value)
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -54,3 +54,61 @@ class Chip8InterpreterTest(unittest.TestCase):
         # Test registers between 0 and 15 (in hexa)
         for i in range(0, 0xf):
             self.assertEqual(interpreter.get_register(i), 0)
+
+    def test_graphic_memory_initialization(self):
+        interpreter = Chip8Interpreter(b'')
+        self.assertEqual(interpreter.get_pixel(0, 0), 0)
+        self.assertEqual(interpreter.get_pixel(63, 31), 0)
+
+    def test_graphic_memory_put_pixel(self):
+        interpreter = Chip8Interpreter(b'')
+        interpreter.put_pixel(42, 17, 1)
+        self.assertEqual(interpreter.get_pixel(42, 17), 1)
+
+    def test_graphic_memory_out_of_bounds(self):
+        interpreter = Chip8Interpreter(b'')
+
+        with self.assertRaises(IndexError) as error:
+            interpreter.get_pixel(64, 31)
+
+        with self.assertRaises(IndexError) as error:
+            interpreter.get_pixel(63, 32)
+
+        with self.assertRaises(IndexError) as error:
+            interpreter.get_pixel(-1, 17)
+
+        with self.assertRaises(IndexError) as error:
+            interpreter.get_pixel(17, -1)
+
+    def test_execute_sprite(self):
+        # 0000: sprite v0 v0 1 (I implicitly gives the sprite address)
+        interpreter = Chip8Interpreter(b'\xD0\x01\x12')
+        # Force the Index register to point to the byte 2
+        # The byte 2 contains the sprite to draw
+        interpreter.I = 2
+        interpreter.step()
+
+        # The drawn sprite should look like: 00010010
+        # Each element is a pixel turned on
+        self.assertPixelLine(interpreter, 0, 0, "00010010")
+
+    def test_execute_two_tall_sprite(self):
+        # 0000: sprite v0 v0 2 (I implicitly gives the sprite address)
+        interpreter = Chip8Interpreter(b'\xD0\x02\x12\x34')
+        # Force the Index register to point to the byte 2
+        # The byte 2 contains the sprite to draw
+        interpreter.I = 2
+        interpreter.step()
+
+        # The drawn sprite should look like:
+        #   00010010
+        #   00110100
+        # Each element is a pixel turned on
+        self.assertPixelLine(interpreter, 0, 0, "00010010")
+        self.assertPixelLine(interpreter, 0, 1, "00110100")
+
+    def assertPixelLine(self, interpreter, x, y, expected_pattern):
+        got_pattern = ""
+        for i in range(x, x + 8):
+            got_pattern += str(interpreter.get_pixel(i, y))
+        self.assertEqual(expected_pattern, got_pattern)
